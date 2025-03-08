99626f44dbed6e22a1053db4da2c7b6b52b72cdd
ayshaAlthawadi12 <aysha.abdulla.althawadi@gmail.com>
2025-03-08 03:48:12 +0300

    Add files via upload

---
+++ src/env.js
@@ -0,0 +1,11 @@
+export const environment = {
+  apiKey: "AIzaSyDYfH4QRhS5sUqfYaTgW5howfvyx8GIH9M",
+  authDomain: "excersicetracker-df794.firebaseapp.com",
+  databaseURL: "https://excersicetracker-df794-default-rtdb.firebaseio.com",
+  projectId: "excersicetracker-df794",
+  storageBucket: "excersicetracker-df794.firebasestorage.app",
+  messagingSenderId: "982206189051",
+  appId: "1:982206189051:web:e5ea26b761a3a47a568f5e",
+  measurementId: "G-B56H6FRBP2"
+
+  };
